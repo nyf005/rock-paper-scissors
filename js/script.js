338b8f715948de10ac59350d39dbafd09f4a1ce9
Fabrice N'DRI <nyf005@gmail.com>
2022-04-30 22:16:31 +0000

    Create a function to play a single round of the game

--- js/script.js
+++ js/script.js
@@ -6,11 +6,11 @@ function computerPlay() {
 
   // Set computer choice based on random number
   if (randomNumber == 0) {
-    computerChoice = "ROCK";
+    computerChoice = "Rock";
   } else if (randomNumber == 1) {
-    computerChoice = "PAPER";
+    computerChoice = "Paper";
   } else {
-    computerChoice = "SCISSORS";
+    computerChoice = "Scissors";
   }
 
   // Return computer choice
@@ -28,3 +28,29 @@ function capitalize(text) {
   // return the capitalized text
   return firstLetter + restOfText;
 }
+
+function playRound(playerSelection, computerSelection) {
+  // Capitalize the playerSelection
+  playerSelection = capitalize(playerSelection);
+  let result;
+
+  if (playerSelection == computerSelection) {
+    result = `It's a tie game!`;
+  } else if (
+    // Here are the only cases in which the player can win
+    (playerSelection == "Paper" && computerSelection == "Rock") ||
+    (playerSelection == "Rock" && computerSelection == "Scissors") ||
+    (playerSelection == "Scissors" && computerSelection == "Paper")
+  ) {
+    result = `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    // Otherwise, the computer wins
+    result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+  }
+
+  return result;
+}
+
+const playerSelection = "ROck";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
